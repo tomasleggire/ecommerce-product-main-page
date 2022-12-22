d79766b26a1065e4d6e32213d630060cc324607d
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-21 21:25:33 -0300

    Navbar desktop terminado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Navbar from './COMPONENTS/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <p>Pito</p>
+      <Navbar />
     </div>
   );
 }
---
+++ src/COMPONENTS/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../CSS/Narbar.css';
+import logo from '../IMAGES/logo.svg';
+import profileImg from '../IMAGES/image-avatar.png';
+import {BsCart3} from "react-icons/bs";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="img-div"><img className='logo-img' src={logo}/></div>
+            <ul>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Woman</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+            <div className="profile-main">
+                <BsCart3 className="cart-logo"/>
+                <div className="profile-img"><img className='avatar-img' src={profileImg}/></div>
+            </div>
+        </nav>
+    )
+}
