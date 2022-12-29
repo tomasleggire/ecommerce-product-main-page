0de5a69403bd2f10a51274ac7b34873bed374034
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-28 21:25:53 -0300

    Esqueleto de photo gallery terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './COMPONENTS/Navbar';
+import PhotoGallery from './COMPONENTS/PhotoGallery';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className='main-contain'>
+        <PhotoGallery />
+      </div>
     </div>
   );
 }
---
+++ src/COMPONENTS/PhotoGallery.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../CSS/PhotoGallery.css';
+
+export default function PhotoGallery() {
+    return (
+        <div className="main-gallery">
+            <div className="main-photo-gallery"></div>
+            <div className="options-container">
+                <div className="option"></div>
+                <div className="option"></div>
+                <div className="option"></div>
+                <div className="option"></div>
+            </div>
+        </div>
+    )
+}
