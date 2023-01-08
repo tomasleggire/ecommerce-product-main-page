78945d32b8a9413add1efb01ecb965e92e56e8c7
Tomás Leggire <tomasleggire@hotmail.com>
2023-01-07 21:50:43 -0300

    ModalCart desktop terminado

--- src/COMPONENTS/CartModal.js
+++ src/COMPONENTS/CartModal.js
@@ -1,10 +1,14 @@
 import React from "react";
 import '../CSS/CartModal.css';
+import { BsXLg } from "react-icons/bs";
 
 export default function CartModal({setCartModalValue}) {
     return (
-        <div className="main-modal-cart" onClick={() => setCartModalValue(false)}>
+        <div className="main-modal-cart">
+            <div className="title-container-cart">
             <h2 className="title-cart">Cart</h2>
+            <BsXLg className="btn-close-cart" onClick={() => setCartModalValue(false)}/>
+            </div>
             <div className="cart-items-container">
                 <span className="span-empty-cart">You cart is empty.</span>
             </div>
